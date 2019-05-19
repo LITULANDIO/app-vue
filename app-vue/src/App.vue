33ca5b3b74bb1213eb8ca53c416f0bc8f865ad07
clitus <clitus@casalsonline.es>
2019-05-20 01:18:55 +0200

    initial app

---
+++ app-vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<persona></persona>
+
+</template>
+
+<script>
+import persona from './components/persona.vue'
+
+export default {
+      components:{ persona },
+      data () {
+          return {
+          
+
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './scss/main.scss';
+
+</style>
